ff7641e1e343e8150653ed04aa48cafec1d59547
Raghuveersinh C Jhala <jhalar@go.stockton.edu>
2018-10-10 23:41:02 -0400

    Add project files.

---
+++ Lab4/Controllers/Lab4Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["timephrase"] = timeOfDay();
+            DateTime date = DateTime.Now;
+            return View(date);
+        }
+
+        public IActionResult Waffles()
+        {
+            ViewData["timephrase"] = timeOfDay();
+            return View("Waffles");
+        }
+
+
+        public string timeOfDay()
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                return "Good Morning";
+            }
+            else if (DateTime.Now.Hour < 18)
+            {
+                return "Good Afternoon";
+            }
+            else
+            {
+                return "Good Evening";
+            }
+        }
+    }
+}
